c7de12e3b28022e83f4b040398412551ce0cf1c9
Danny <perez6736@gmail.com>
2025-01-25 23:14:42 +0900

    commit from intellij

--- src/test/java/dannyAppiumCourse/AppiumBasics.java
+++ src/test/java/dannyAppiumCourse/AppiumBasics.java
@@ -1,11 +1,17 @@
 package dannyAppiumCourse;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AppiumBasics {
 	
 	@Test
-	public void AppiumTest() {
+	public void AppiumTest() throws MalformedURLException {
+		
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), null);
 		
 	}
 
